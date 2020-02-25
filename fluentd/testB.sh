7dccabc55dc74858956c1094a7576804ca9b7d69
vishnu.madduri@gmail.com <Reddy102629>
2020-02-25 08:26:20 +0530

    client.html dynamic div creation

--- fluentd/testB.sh
+++ fluentd/testB.sh
@@ -2,8 +2,8 @@
 for i in {0..300..10}
 do
 echo "Welcome $i times"
-JSON_FMT='{"did":"%s","accel {x":"%d","y":"%d","z":"%d}"}\n'
-json_string=$(printf "$JSON_FMT" "alpha" $i $i 2)
+JSON_FMT='{"did":"%s","accel" :{"x":"%d","y":"%d","z":"%d"}}\n'
+json_string=$(printf "$JSON_FMT" "beta" $i $i 2)
 echo "$json_string"
 echo "$json_string" | fluent-cat redis.vishnu1
 
